e9c50f0a286b739a62ea137c257e1b61dd639e78
Igor <igorhenrikg1@gmail.com>
2024-10-05 13:43:29 -0300

    feat: adiciona setup do banco de dados e rota de cadastro de usuario

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 
 export async function createAccount(app: FastifyInstance){
   app.withTypeProvider<ZodTypeProvider>().post('/users', {
@@ -11,7 +13,25 @@ export async function createAccount(app: FastifyInstance){
         password: z.string().min(6)
       })
     }
-  }, () =>{
-    return 'User Created'
+  }, async (request, reply) =>{
+    const {email, name, password} = request.body
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {email}
+    })
+
+    if(userWithSameEmail){
+      return reply.status(400).send({message: 'user with same email already exists'})
+    }
+
+    const passwordHash = await hash(password, 6)
+
+    await prisma.user.create({
+      data:{
+        name,
+        email,
+        passwordHash
+      }
+    })
+    return reply.status(201).send()
   })
 }
