1d32c685be5f786b340fd23c5ff43f9c3c502fc7
Igor <igorhenrikg1@gmail.com>
2024-10-05 14:49:08 -0300

    feat: adiciona setup do swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,7 +1,9 @@
 import {fastify} from "fastify";
 import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider
@@ -11,6 +13,21 @@ import { createAccount } from "./routes/auth/create-account";
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS app with multi-tenant & RBAC.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
+})
+
 app.register(fastifyCors)
 app.register(createAccount)
 app.listen({port: 3333}).then(() => {
