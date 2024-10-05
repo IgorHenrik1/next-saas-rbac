c94d934d4e680e497846825a6ca65dde2447ca76
Igor <igorhenrikg1@gmail.com>
2024-10-05 15:25:07 -0300

    feat: adiciona rota de autenticação via senha

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,6 +2,7 @@ import {fastify} from "fastify";
 import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifyJwt from "@fastify/jwt";
 import {
   jsonSchemaTransform,
   serializerCompiler,
@@ -9,6 +10,7 @@ import {
   ZodTypeProvider
 } from 'fastify-type-provider-zod'
 import { createAccount } from "./routes/auth/create-account";
+import { authenticateWithPassword } from "./routes/auth/authenticate-with-password";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
@@ -28,8 +30,13 @@ app.register(fastifySwaggerUi, {
   routePrefix: '/docs'
 })
 
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
 app.register(fastifyCors)
 app.register(createAccount)
+app.register(authenticateWithPassword)
 app.listen({port: 3333}).then(() => {
   console.log('Server is running on port 3333')
 })
