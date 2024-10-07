9b008aaf1c764f8a40e521dd748c62c4da73346b
Igor <igorhenrikg1@gmail.com>
2024-10-07 16:04:46 -0300

    feat: adiciona rota de troca de senha

---
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -0,0 +1,56 @@
+import { prisma } from "@/lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { BadRequestError } from "../__errors/bad-request-error";
+import { auth } from "@/http/middlewares/auth";
+import { UnauthorizedError } from "../__errors/unauthorized-error";
+import { hash } from "bcryptjs";
+
+
+export async function resetPassword( app: FastifyInstance){
+  app.withTypeProvider<ZodTypeProvider>().post('/password/reset', {
+    schema: {
+      tags: ['auth'],
+      summary: 'Get authenticated user profile',
+      body: z.object({
+        code: z.string(),
+        password: z.string().min(6)
+      }),
+      response:{
+        204: z.null(),
+      } 
+    }
+  }, async (request, reply) => {
+    const {code, password} = request.body
+    const tokenFromCode = await prisma.token.findUnique({
+      where: {
+        id: code
+      },
+    })
+
+    if(!tokenFromCode){
+      // We don't want people to know if user really exists
+      throw new UnauthorizedError()
+    }
+
+    const passwordHash = await hash(password, 6)
+
+    await prisma.user.update({
+      where: {
+        id: tokenFromCode.userId
+      },
+      data: {
+        passwordHash
+      }
+    })
+
+    return reply.status(204).send()
+
+    // Send e-mail with password recover link
+
+    console.log('Recover password token: ', code)
+    return reply.status(201).send()
+  })
+
+}
