8e48ccbe887e9aeee6e69e29f66cf8010d15b1d9
Igor <igorhenrikg1@gmail.com>
2024-10-10 08:32:27 -0300

    feat: adiciona página de autenticacão

---
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+import githubIcon from "@/assets/github-icon.svg"
+import Image from "next/image";
+
+export default function SignPage(){
+  return(
+    <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+        <Link href="/auth/forgot-password" className="text-xs font-medium text-foreground hover:underline">Forgot your password?</Link>
+      </div>
+
+    <Button type="submit" className="w-full">Sign in with e-mail</Button>
+    <Separator />
+    <Button type="submit" className="w-full" variant="outline">
+      <Image src={githubIcon} className="size-4 mr-2 dark:invert" alt=""/>
+      Sign in with GitHub
+    </Button>
+
+    </form>
+  )
+}
